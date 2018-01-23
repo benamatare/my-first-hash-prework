f5e266f1a92ba19a5227382a5e9b4d217fbd1bba
Ben Matare <ben.a.matare@gmail.com>
2018-01-23 02:57:59 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,11 +4,12 @@ end
 
 
 def shipping_manifest
-  #set a variable called `shipping_manifest`, equal to a hash
-  #fill that hash with key/value pairs that describe the following information:
-  #We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
+  shipping_manifest = hash_inside
+  hash_inside = {"whale bone corsets" => 5, "porcelain vases" = > 2, "oil paintings" => 3}
 end
-
+#set a variable called `shipping_manifest`, equal to a hash
+#fill that hash with key/value pairs that describe the following information:
+#We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
 def retrieval
   #your code here!
 end
