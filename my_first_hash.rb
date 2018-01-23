8a96d26b00275ce09350051b2754d8db1e05a2d1
Ben Matare <ben.a.matare@gmail.com>
2018-01-23 02:59:14 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -6,8 +6,8 @@ end
 
 
 def shipping_manifest
-  hash_inside = shipping_manifest
-  hash_inside = {
+
+  shipping_manifest = {
     "whale bone corsets" => 5,
     "porcelain vases" => 2,
     "oil paintings" => 3
