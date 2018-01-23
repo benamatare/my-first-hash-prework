0a185de7652b41ec4c4265ac1bf8bed4c27c893f
Ben Matare <ben.a.matare@gmail.com>
2018-01-23 02:56:26 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,5 @@
 def my_hash
-# use the literal constructor to set the variable,
-# my_hash, equal to a hash with key/value pairs of your choice.
-hash_inside = {"key","value"}
+  hash_inside = {"key","value"}
 end
 
 
@@ -18,7 +16,7 @@ end
 def adding
   #shipping_manifest["muskets"] = 2
   #shipping_manifest["gun powder"] = 4
-  return 
+  return
   #your code here
   #remember to return the shipping_manifest hash
 
