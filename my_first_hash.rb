ff0b6dbe68767515d3747431c29ff641268d29e6
Ben Matare <ben.a.matare@gmail.com>
2018-01-23 03:02:53 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,9 +23,15 @@ return shipping_manifest["oil paintings"]
 end
 
 def adding
+  shipping_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
+
+    shipping_manifest["muskets" => 2]["gun powder" => 4]
   #shipping_manifest["muskets"] = 2
   #shipping_manifest["gun powder"] = 4
-  return
+  return shipping_manifest
   #your code here
   #remember to return the shipping_manifest hash
 
